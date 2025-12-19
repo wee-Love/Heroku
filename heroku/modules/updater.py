f16e7466bf9ca466affacf57d4fa3b6aa87847a6
Who? <155328415+coddrago@users.noreply.github.com>
2025-12-19 15:26:40 +0000

    remove useless strings and fix bot answer with autoupdate

--- heroku/modules/updater.py
+++ heroku/modules/updater.py
@@ -70,14 +70,12 @@ class UpdaterMod(loader.Module):
         self.set("autoupdate", True)
         if not state:
             self.config["autoupdate"] = False
-            await self.inline.bot(call.answer(self.strings("autoupdate_off").format(prefix=self.get_prefix()), show_alert=True))
-            await call.delete()
+            await self.inline.bot(call.edit(self.strings("autoupdate_off").format(prefix=self.get_prefix())))
             return
         
         self.config["autoupdate"] = True
 
-        await self.inline.bot(call.answer(self.strings("autoupdate_on"), show_alert=True))
-        await call.delete()
+        await self.inline.bot(call.edit(self.strings("autoupdate_on")))
 
     def get_changelog(self) -> str:
         try:
