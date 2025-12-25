ad2c1ada7e19a6e08daff768aadc9bfe6a730563
Who? <155328415+coddrago@users.noreply.github.com>
2025-12-25 11:24:13 +0000

    add help -c and help -l (gcode, i will rework this a bit later)

--- heroku/modules/help.py
+++ heroku/modules/help.py
@@ -264,6 +264,16 @@ class Help(loader.Module):
             args = args.replace(" -f", "").replace("-f", "")
             force = True
 
+        only_core = False
+        if "-c" in args:
+            args = args.replace(" -c", "").replace("-c", "")
+            only_core = True
+
+        only_loaded = False
+        if "-l" in args:
+            args = args.replace(" -l", "").replace("-l", "")
+            only_loaded = True
+
         if args:
             await self.modhelp(message, args)
             return
@@ -365,21 +375,52 @@ class Help(loader.Module):
         core_.sort(key=str.lower)
         no_commands_.sort(key=str.lower)
 
-        await utils.answer(
-            message,
-            (self.config["desc_icon"] + " {}\n <blockquote expandable>{}</blockquote><blockquote expandable>{}</blockquote><blockquote expandable>{}</blockquote>").format(
-                reply,
-                "".join(core_),
-                "".join(plain_ + (no_commands_ if force else [])),
-                (
-                    ""
-                    if self.lookup("Loader").fully_loaded
-                    else f"\n\n{self.strings('partial_load')}"
+        if only_core:
+            await utils.answer(
+                message,
+                (self.config["desc_icon"] + " {}\n <blockquote expandable>{}</blockquote><blockquote expandable>{}</blockquote>").format(
+                    reply,
+                    "".join(core_),
+                    (
+                        ""
+                        if self.lookup("Loader").fully_loaded
+                        else f"\n\n{self.strings('partial_load')}"
+                    ),
                 ),
-            ),
-            file = banner,
-            invert_media = self.config["invert_media"],
-        )
+                file = banner,
+                invert_media = self.config["invert_media"],
+            )
+        elif only_loaded:
+            await utils.answer(
+                message,
+                (self.config["desc_icon"] + " {}\n <blockquote expandable>{}</blockquote><blockquote expandable>{}</blockquote>").format(
+                    reply,
+                    "".join(plain_ + (no_commands_ if force else [])),
+                    (
+                        ""
+                        if self.lookup("Loader").fully_loaded
+                        else f"\n\n{self.strings('partial_load')}"
+                    ),
+                ),
+                file = banner,
+                invert_media = self.config["invert_media"],
+            )
+        else:
+            await utils.answer(
+                message,
+                (self.config["desc_icon"] + " {}\n <blockquote expandable>{}</blockquote><blockquote expandable>{}</blockquote><blockquote expandable>{}</blockquote>").format(
+                    reply,
+                    "".join(core_),
+                    "".join(plain_ + (no_commands_ if force else [])),
+                    (
+                        ""
+                        if self.lookup("Loader").fully_loaded
+                        else f"\n\n{self.strings('partial_load')}"
+                    ),
+                ),
+                file = banner,
+                invert_media = self.config["invert_media"],
+            )
 
     @loader.command(ru_doc="| Ссылка на чат помощи", ua_doc="| посилання для чату служби підтримки", de_doc="| Link zum Support-Chat")
     async def support(self, message):
